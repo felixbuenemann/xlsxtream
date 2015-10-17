af3d6dec3ac16944f544941434364ae6679b2a20
Felix Bünemann <buenemann@louis.info>
2015-10-17 17:39:01 +0200

    Add custom deflater to fix rubyzip buffering
    This is a workaround for the issue described in rubyzip/rubyzip#233
    and can be removed once a rubyzip version with rubyzip/rubyzip#250
    is released.

--- lib/xlsxtream/io/rubyzip.rb
+++ lib/xlsxtream/io/rubyzip.rb
@@ -5,7 +5,7 @@ module Xlsxtream
     class RubyZip
       def initialize(path_or_io)
         stream = path_or_io.respond_to? :reopen
-        @zos = ::Zip::OutputStream.new(path_or_io, stream)
+        @zos = UnbufferedZipOutputStream.new(path_or_io, stream)
       end
 
       def <<(data)
@@ -18,6 +18,46 @@ module Xlsxtream
 
       def close
         @zos.close
+      end
+
+      # Extend get_compressor to hook our custom deflater.
+      class UnbufferedZipOutputStream < ::Zip::OutputStream
+        private def get_compressor(entry, level)
+          case entry.compression_method
+          when ::Zip::Entry::DEFLATED then
+            StreamingDeflater.new(@output_stream, level, @encrypter)
+          else
+            super
+          end
+        end
+      end
+
+      # RubyZip's Deflater buffers to a StringIO until finish is called.
+      # This StreamingDeflater writes out chunks during compression.
+      class StreamingDeflater < ::Zip::Compressor
+        def initialize(output_stream, level = Zip.default_compression, encrypter = NullEncrypter.new)
+          super()
+          @output_stream = output_stream
+          @zlib_deflater = ::Zlib::Deflate.new(level, -::Zlib::MAX_WBITS)
+          @size          = 0
+          @crc           = ::Zlib.crc32
+          unless encrypter.is_a? ::Zip::NullEncrypter
+            raise ::Zip::Error, 'StreamingDeflater does not support encryption'
+          end
+        end
+
+        def <<(data)
+          val   = data.to_s
+          @crc  = Zlib.crc32(val, @crc)
+          @size += val.bytesize
+          @output_stream << @zlib_deflater.deflate(data)
+        end
+
+        def finish
+          @output_stream << @zlib_deflater.finish until @zlib_deflater.finished?
+        end
+
+        attr_reader :size, :crc
       end
     end
   end
