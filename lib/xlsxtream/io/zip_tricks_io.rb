b6145cb8089ce37f42041743f923e51ed7585ae8
Julik Tarkhanov <me@julik.nl>
2017-10-30 15:56:33 +0100

    Update to ZipTricks with deferred writes

--- lib/xlsxtream/io/zip_tricks_io.rb
+++ lib/xlsxtream/io/zip_tricks_io.rb
@@ -8,26 +8,17 @@ module Xlsxtream
       end
 
       def <<(data)
-        @wf.resume(data)
+        @wf << data
         self
       end
 
       def add_file(path)
-        @wf.resume(:__close__) if @wf
-
-        @wf = Fiber.new do | bytes_or_close_sym |
-          @streamer.write_deflated_file(path) do |write_sink|
-            loop do
-              break if bytes_or_close_sym == :__close__
-              write_sink << bytes_or_close_sym
-              bytes_or_close_sym = Fiber.yield
-            end
-          end
-        end
+        @wf.close if @wf
+        @wf = @streamer.write_deflated_file(path)
       end
 
       def close
-        @wf.resume(:__close__) if @wf
+        @wf.close if @wf
         @streamer.close
       end
     end
--- xlsxtream.gemspec
+++ xlsxtream.gemspec
@@ -20,7 +20,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
   spec.required_ruby_version = ">= 2.1.0"
 
-  spec.add_dependency "zip_tricks", "~> 4"
+  spec.add_dependency "zip_tricks", "~> 4.5"
 
   spec.add_development_dependency "bundler", "~> 1.7"
   spec.add_development_dependency "rake", "~> 10.0"
