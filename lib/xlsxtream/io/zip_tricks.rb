771e58bec7012e89fa7713d1f446e212cd7d90dd
Felix Bünemann <buenemann@louis.info>
2017-10-31 01:00:39 +0100

    Buffer 64k in ZipTricks IO to improve performance
    The zip_tricks gem uses less internal buffering than RubyZip, so wee
    need to do some buffering of our own to get equal or better performance
    than with the RubyZip IO for small rows.

--- lib/xlsxtream/io/zip_tricks.rb
+++ lib/xlsxtream/io/zip_tricks.rb
@@ -3,24 +3,41 @@ require "zip_tricks"
 module Xlsxtream
   module IO
     class ZipTricks
+      BUFFER_SIZE = 64 * 1024
+
       def initialize(body)
         @streamer = ::ZipTricks::Streamer.new(body)
         @wf = nil
+        @buffer = ''
       end
 
       def <<(data)
-        @wf << data
+        @buffer << data
+        flush_buffer if @buffer.size >= BUFFER_SIZE
         self
       end
 
       def add_file(path)
-        @wf.close if @wf
+        flush_file
         @wf = @streamer.write_deflated_file(path)
       end
 
       def close
-        @wf.close if @wf
+        flush_file
         @streamer.close
+      end
+
+      private
+
+      def flush_buffer
+        @wf << @buffer
+        @buffer.clear
+      end
+
+      def flush_file
+        return unless @wf
+        flush_buffer if @buffer.size > 0
+        @wf.close
       end
     end
   end
